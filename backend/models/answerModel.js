06a929b756cc06fa0efc2863ffe729ff44453535
TLiberis <tautvydaslbrs@yahoo.com>
2023-07-06 21:11:04 +0300

    pridedu answer handlinima

---
+++ backend/models/answerModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const answerSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  question: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Question',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model('Answer', answerSchema);
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import { userLogin, userRegister } from '../controllers/userController.js';
+import {
+  createQuestion,
+  deleteQuestion,
+  getQuestions,
+} from '../controllers/questionController.js';
+import { validateToken } from '../middleware/authUser.js';
+import {
+  createAnswer,
+  deleteAnswer,
+  getAnswers,
+} from '../controllers/answerController.js';
 
 const router = express.Router();
 
 router.post('/register', userRegister);
 router.post('/login', userLogin);
 
-router.get('/questions');
-router.post('/question');
-router.delete('/question/:id');
+router.get('/questions', getQuestions);
+router.post('/question', validateToken, createQuestion);
+router.delete('/question/:id', validateToken, deleteQuestion);
 
-router.get('/question/:id/answers');
-router.post('/question/:id/answers');
-router.delete('/answer/:id');
+router.get('/question/:id/answers', getAnswers);
+router.post('/question/:id/answers', validateToken, createAnswer);
+router.delete('/answer/:id', validateToken, deleteAnswer);
 
 export default router;
